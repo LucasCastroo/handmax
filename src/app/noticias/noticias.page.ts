588ba81650d9ba0399cc8fb9b826dff8066d1f94
JpBllack <joaogomes@unitins.br>
2024-10-20 19:51:48 -0300

    tela de CRUD de noticias arrumando bugs

--- src/app/noticias/noticias.page.ts
+++ src/app/noticias/noticias.page.ts
@@ -8,43 +8,49 @@ import { Component, OnInit } from '@angular/core';
 export class NoticiasPage implements OnInit {
   news: any = {
     title: '',
-    image: '',
-    paragraph: ''
+    images: [],
+    paragraphs: []
   };
 
-  currentField: string = '';
-  isPreview: boolean = false;
-  isNewsList: boolean = false;
-  newsList: any[] = [];
+  entryOrder: string[] = []; // Array para gerenciar a ordem de exibição
+  isTitleVisible: boolean = false;
 
   constructor() {}
 
   ngOnInit() {}
 
-  addField(field: string) {
-    this.currentField = field;
-    this.isPreview = false;
+  addTitle() {
+    this.isTitleVisible = true; // Exibe o título
+    this.entryOrder.push('title'); // Adiciona à ordem
+  }
+
+  addImage() {
+    this.news.images.push(''); // Adiciona um novo espaço para imagem
+    this.entryOrder.push('image'); // Adiciona à ordem
+  }
+
+  addParagraph() {
+    this.news.paragraphs.push(''); // Adiciona um novo espaço para parágrafo
+    this.entryOrder.push('paragraph'); // Adiciona à ordem
+  }
+
+  removeTitle() {
+    this.isTitleVisible = false; // Remove o título
+    this.news.title = ''; // Limpa o título
+    this.entryOrder = this.entryOrder.filter(entry => entry !== 'title'); // Remove da ordem
+  }
+
+  removeImage(index: number) {
+    this.news.images.splice(index, 1); // Remove a imagem do array
+    this.entryOrder.splice(this.entryOrder.indexOf('image'), 1); // Remove da ordem, garantindo que a ordem de exibição seja atualizada
+  }
+
+  removeParagraph(index: number) {
+    this.news.paragraphs.splice(index, 1); // Remove o parágrafo do array
+    this.entryOrder.splice(this.entryOrder.indexOf('paragraph'), 1); // Remove da ordem
   }
 
   previewNews() {
-    this.isPreview = true;
-  }
-
-  saveNews() {
-    this.newsList.push({ ...this.news });
-    this.news = {
-      title: '',
-      image: '',
-      paragraph: ''
-    };
-    this.isPreview = false;
-  }
-
-  showNewsList() {
-    this.isNewsList = true;
-  }
-
-  goBackToEditor() {
-    this.isNewsList = false;
+    // Lógica para visualização das notícias, se necessário
   }
 }
