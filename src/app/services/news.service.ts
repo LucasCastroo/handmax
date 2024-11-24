5eeaaa41fe70f93d30692acf4ef81072ed284022
JpBllack <joaogomes@unitins.br>
2024-11-24 11:21:01 -0300

    atualização do news.service

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { PublicacaoDTO } from '../models/publicacao-dto.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +12,43 @@ export class NewsService {
 
   constructor(private http: HttpClient) {}
 
-  // Método para obter todas as notícias
-  getNews(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  // Método para obter todas as notícias com paginação
+  getNews(page: number = 0, pageSize: number = 100): Observable<PublicacaoDTO[]> {
+    const params = { page: page.toString(), pageSize: pageSize.toString() };
+    return this.http.get<PublicacaoDTO[]>(this.apiUrl, { params });
+  }
+
+  // Método para obter uma única notícia por ID
+  getNewsById(id: number): Observable<PublicacaoDTO> {
+    return this.http.get<PublicacaoDTO>(`${this.apiUrl}/${id}`);
   }
 
   // Método para criar uma nova notícia
-  createNews(news: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, news);
+  createNews(news: PublicacaoDTO): Observable<PublicacaoDTO> {
+    return this.http.post<PublicacaoDTO>(this.apiUrl, news);
   }
 
   // Método para atualizar uma notícia existente
-  updateNews(id: string, news: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, news);
+  updateNews(id: number, news: PublicacaoDTO): Observable<PublicacaoDTO> {
+    return this.http.put<PublicacaoDTO>(`${this.apiUrl}/${id}`, news);
   }
 
   // Método para excluir uma notícia
-  deleteNews(id: string): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  deleteNews(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  // Método para fazer upload de imagem
+  uploadImage(id: number, formData: FormData): Observable<void> {
+    return this.http.patch<void>(`${this.apiUrl}/upload/imagem/${id}`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+  }
+
+  // Método para fazer download de imagem
+  downloadImage(nomeImagem: string): Observable<Blob> {
+    return this.http.get(`${this.apiUrl}/download/imagem/${nomeImagem}`, { responseType: 'blob' });
   }
 }
